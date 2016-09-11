436115782f406ef83de08aaf1080bd42f6b2e74e
buptwangzhiwen <1063883112@qq.com>
2016-09-11 09:22:35 +0800

    时间：2016-9-10 编辑者：汪志文 在会议预定系统中的documentInfo.jsp页面中的下载按钮的右边添加一个删除按钮，他的作用是：1、将上传到本地文件夹C:\conference\file\upload中的对应文件删除。2、将数据库file表中的对应的文件信息删除。3、利用ajax技术能对部分页面刷新的功能，将页面显示的文件信息刷新。

---
+++ src/com/zhang/file/FileDeleteServlet.java
@@ -0,0 +1,91 @@
+package com.zhang.file;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zhang.dao.MysqlAction;
+
+public class FileDeleteServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public FileDeleteServlet() {
+		super();
+	}
+ 
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String fileName = request.getParameter("text");
+		String PathName = "C:\\conference\\file\\" + "upload\\" + fileName;
+		File file = new File(PathName);
+		file.delete(); // 删除本地的文件
+
+		MysqlAction mysqlAction = new MysqlAction();
+		try {
+			mysqlAction.deleteFileByFileName(fileName); // 通过文件名删除数据库文件信息
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect("documentInfo.jsp");
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+}
